ac34c34d43bc59705515741e10b4418846084450
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-07-09 14:28:48 -0600

    a function for getting the path is now added

--- dataprepro/prepro.py
+++ dataprepro/prepro.py
@@ -5,12 +5,19 @@ import os
 import pathlib
 import constants
 
-path_un_data = pathlib.Path(constants.UN_DATA_PATH).absolute()
-path_raw_data = pathlib.Path(constants.RAW_DATA_PATH).absolute()  # resolving the path and getting
+
+def get_absolute_path(path):
+    return pathlib.Path(path).absolute()
+
+
+path_un_data = get_absolute_path(constants.UN_DATA_PATH)
+path_raw_data = get_absolute_path(constants.RAW_DATA_PATH)  # resolving the path and getting
 # the correct path
 supported_prog_langs = os.listdir(path_raw_data)  # we suppose for each programing
 # language there is a directory with its name in the raw data
+path_spl = []
 
+for path in supported_prog_langs:
+    path_spl.append(get_absolute_path(str(path_raw_data)+'/'+str(path)))
 
-
-
+print(path_spl)
