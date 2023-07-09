05915551ebb0561cec6ab37c538cc75c337509ac
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-07-09 14:24:31 -0600

    the needed paths are loaded

--- dataprepro/constants.py
+++ dataprepro/constants.py
@@ -1,6 +1,6 @@
 SYMBOLS = """~`!@#$%^&*()-+|\}{]["':=;?/>< """
 SYMBOLS_ = """~`!@#$%^&*()-+|\}{]["':=;?/><""" # without space
 
-RAW_DATA_PATH = "plr/raw-data"
-UN_DATA_PATH = "plr/un-data"
+RAW_DATA_PATH = "raw-data"
+UN_DATA_PATH = "un-data"
 
--- dataprepro/prepro.py
+++ dataprepro/prepro.py
@@ -1,3 +1,16 @@
 """this is the file that the data preprocessing will be
 donne here. so basically we upload all the files then we
 extract the syntax, and then we write into the un-data folder"""
+import os
+import pathlib
+import constants
+
+path_un_data = pathlib.Path(constants.UN_DATA_PATH).absolute()
+path_raw_data = pathlib.Path(constants.RAW_DATA_PATH).absolute()  # resolving the path and getting
+# the correct path
+supported_prog_langs = os.listdir(path_raw_data)  # we suppose for each programing
+# language there is a directory with its name in the raw data
+
+
+
+
