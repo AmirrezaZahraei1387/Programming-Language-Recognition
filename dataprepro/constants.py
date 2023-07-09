a9fe2b2613185e923bfd1d14c25f1c34ce7aea91
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-07-09 14:16:42 -0600

    the pathes are added to constants.py

--- dataprepro/constants.py
+++ dataprepro/constants.py
@@ -1,2 +1,6 @@
 SYMBOLS = """~`!@#$%^&*()-+|\}{]["':=;?/>< """
 SYMBOLS_ = """~`!@#$%^&*()-+|\}{]["':=;?/><""" # without space
+
+RAW_DATA_PATH = "plr/raw-data"
+UN_DATA_PATH = "plr/un-data"
+
---
+++ dataprepro/prepro.py
@@ -0,0 +1,3 @@
+"""this is the file that the data preprocessing will be
+donne here. so basically we upload all the files then we
+extract the syntax, and then we write into the un-data folder"""
