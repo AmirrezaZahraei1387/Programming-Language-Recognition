fe2a2063223ca3ab366a373002b261afdb40bc7d
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-07-09 14:39:18 -0600

    the needed paths are uploaded

--- dataprepro/prepro.py
+++ dataprepro/prepro.py
@@ -10,14 +10,40 @@ def get_absolute_path(path):
     return pathlib.Path(path).absolute()
 
 
+def get_all_absolute_path(names_elements, direct):
+
+    pathspl = []
+    for p in names_elements:
+        pathspl.append(get_absolute_path(str(direct) + '/' + str(p)))
+    return pathspl
+
+
+# uploading all the needed paths to work with
 path_un_data = get_absolute_path(constants.UN_DATA_PATH)
 path_raw_data = get_absolute_path(constants.RAW_DATA_PATH)  # resolving the path and getting
 # the correct path
 supported_prog_langs = os.listdir(path_raw_data)  # we suppose for each programing
 # language there is a directory with its name in the raw data
-path_spl = []
+path_spl = get_all_absolute_path(supported_prog_langs, path_raw_data)
+path_sub_spl = []
+for p in path_spl:
+    names = os.listdir(p)
+    path_sub_spl.append(get_all_absolute_path(names, p))
 
-for path in supported_prog_langs:
-    path_spl.append(get_absolute_path(str(path_raw_data)+'/'+str(path)))
+print("path_un_data:", path_un_data)
+print("path_raw_data:", path_raw_data)
+print("supported_prog_langs:", supported_prog_langs)
+print("path_spl:", path_spl)
+print("path_sub_spl", path_sub_spl)
+# ===============================================
 
-print(path_spl)
+
+
+
+
+
+
+
+
+
+
