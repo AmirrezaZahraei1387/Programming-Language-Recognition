4925950fb5c68c78cb9591c92080f2b09807095b
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-07-10 16:26:17 -0600

    some tests

--- dataprepro/prepro.py
+++ dataprepro/prepro.py
@@ -5,8 +5,6 @@ extract the syntax, and then we write into the un-data folder"""
 import os
 import pathlib
 import constants
-from numpy import array
-from numpy import where, append, delete
 import tokenizer
 
 
@@ -30,6 +28,14 @@ def open_file(path):
     return data
 
 
+def search(array, item):
+
+    index = -1
+    for element in array:
+        index += 1
+        if element[0] == item:
+            return index
+
 
 # uploading all the needed paths to work with
 path_un_data = get_absolute_path(constants.UN_DATA_PATH)
@@ -49,6 +55,52 @@ print("supported_prog_langs:", supported_prog_langs)
 print("path_spl:", path_spl)
 print("path_sub_spl", path_sub_spl)
 # ===============================================
+
+index = -1
+
+for lang in supported_prog_langs:
+
+    index += 1
+    path_new = get_absolute_path(str(path_un_data)+'/'+str(lang))
+    os.mkdir(path_new)
+    tokenized_texts = []
+    print("programing language:", lang)
+    for path_raw_data in path_sub_spl[index]:
+
+        data = open_file(path_raw_data)
+
+        for statement in data:
+            if statement != "\n" or statement != "":    # checking if the line is empty or no
+
+                t = tokenizer.tokenize(statement[:-1])
+                result = tokenizer.generalformataddspace(t)
+                search_result = search(tokenized_texts, result[0])  # it can be none or the index of the object
+
+                if search_result is None:
+                    tokenized_texts.append([result[0],[result[1]]])
+
+                else:
+                    tokenized_texts[search_result][1].append(result[1])
+
+    # here the work for that language is done, and so
+    # we go to do another one that exist
+
+    path_text_file = get_absolute_path(str(path_new)+"/"+"data.txt")
+    with open(path_text_file, "a") as file:
+        file.write(str(tokenized_texts))
+    file.close()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ dataprepro/raw-data/c++/r.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<math>
+#include<cstddef>
+#include<ofstream>
+#include<json>
+
+int add(int i, int m){
+return i+m;
+}
+
+
+int main(){
+
+return 0;
+}
