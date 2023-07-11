d29a5e860942017022e37e817e50ae356e71fe5b
amirrezazahraei <amirreza.zahraei1@rbe.sk.ca>
2023-07-11 13:53:27 -0600

    the file is moved to prero and it is added to the __init__.py

--- dataprepro/__init__.py
+++ dataprepro/__init__.py
@@ -1,3 +1,4 @@
 
 import dataprepro.tokenizer as tokenizer
 import dataprepro.path_manager as path_manager
+from dataprepro.data_opener import DataFileOpener
--- dataprepro/data_opener.py
+++ dataprepro/data_opener.py
@@ -4,12 +4,14 @@ because we won't get messy in the main file."""
 
 import os
 import pathlib
-
+import dataprepro.path_manager as path_manager
+import dataprepro.constants as constants
 
 class DataFileOpener:
 
-    def __init__(self, path=constants.DAtA_FILE):
-
+    def __init__(self):
+        obj = path_manager.PathManager(constants.RAW_DATA_PATH, constants.UN_DATA_PATH)
+        self.path = obj.path_un_data
         self.supported_langs = os.listdir(self.path)
 
     def make_path(self, lang):
@@ -17,20 +19,15 @@ class DataFileOpener:
 
         if lang not in self.supported_langs:
             raise NotImplementedError("the lang expected is not implemented")
-        return self.path+str("/")+lang+"/"+"data.txt"
+        return str(self.path)+str("/")+lang+"/"+"data.txt"
 
     def open_langs(self, lang):
         """opening the data using a language"""
         with open(self.make_path(lang), mode="r") as file:
-            data = list(file.read())
+            data = eval(file.read())
         file.close()
         return data
 
     def open_number(self, num):
         """opening the data using numbers"""
         return self.open_langs(self.supported_langs[num])
-
-
-a = DataFileOpener()
-
-
