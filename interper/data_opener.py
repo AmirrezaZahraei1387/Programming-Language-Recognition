624e2efb87b56bddd1d696c8863107518ef3577c
amirrezazahraei <amirreza.zahraei1@rbe.sk.ca>
2023-07-11 13:33:20 -0600

    the make_path method is created

--- interper/data_opener.py
+++ interper/data_opener.py
@@ -3,12 +3,28 @@ to open the data for us. it can make every thing very better
 because we won't get messy in the main file."""
 
 import interper.constants as constants
+import os
 
 
 class DataFileOpener:
 
     def __init__(self, path=constants.DAtA_FILE):
+        self.path = path
+        self.supported_langs = os.listdir(self.path)
 
-        path = path
+    def make_path(self, lang):
+        """this method wil make the path of data.txt by getting a language"""
+
+        if lang not in self.supported_langs:
+            raise NotImplementedError("the lang expected is not implemented")
+        return self.path+str("/")+lang+"/"+"data.txt"
+
+    def open_langs(self, lang):
+        pass
+
+
+
+
+
 
 
